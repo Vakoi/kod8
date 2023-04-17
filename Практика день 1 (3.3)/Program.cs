19d16d12ce2d3347fce631d83bbb1d9ad225b33b
Vakoi <mak-uk@mail.ru>
2023-04-17 16:13:26 +0300

    Добавьте файлы проекта.

---
+++ Практика день 1 (3.3)/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Введите число A: ");
+        int a = int.Parse(Console.ReadLine());
+
+        Console.Write("Введите число B: ");
+        int b = int.Parse(Console.ReadLine());
+
+        int sum = 0;
+        for (int i = a; i <= b; i++)
+        {
+            sum += i * i;
+        }
+
+        Console.WriteLine($"Сумма квадратов от {a} до {b} равна {sum}");
+    }
+}
